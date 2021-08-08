2ab3c7a49be03667045d96435130405ae95a5670
Marcus Talken <operationaldev@gmail.com>
2021-08-08 12:55:42 +0000

    Missing dependency and resource name incorrect

--- metadata.rb
+++ metadata.rb
@@ -7,8 +7,10 @@ description       'Installs and configures dnsmasq'
 chef_version      '>= 15.3'
 source_url        'https://github.com/sous-chefs/dnsmasq'
 issues_url        'https://github.com/sous-chefs/dnsmasq/issues'
-version           '1.0.1'
+version           '1.0.2'
 
 %w(ubuntu debian redhat centos oracle).each do |os|
   supports os
 end
+
+depends 'hostsfile', '>= 3.0.1'
